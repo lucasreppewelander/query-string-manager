f004dd15f6ce89f594e85cff2bc0045bd5aeb025
Lucas Reppe Welander <lucas@verismohr.com>
2020-08-17 15:48:34 +0200

    fixed bug

--- test/test.js
+++ test/test.js
@@ -209,6 +209,14 @@ describe('Objectify method',function() {
         var obj = {a:1337, b:'0adad40336ab45b78635bf8fc8b4a824'};
         assert.deepEqual(ret,obj);
         done();
+	});
+	
+	it('Check parsed object uuid and float numbers', function(done) {
+        var url = 'http://www.url.com/?a=13.37&b=0adad40336ab45b78635bf8fc8b4a824';
+        var ret = qsm.objectify(url);
+        var obj = {a:13.37, b:'0adad40336ab45b78635bf8fc8b4a824'};
+        assert.deepEqual(ret,obj);
+        done();
     });
 
     it('Should not parse strings as Infinity', function(done) {
